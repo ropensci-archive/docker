8c08cc06df05c7fc3f13e3c39bfa0e2d55c6b053
Carl Boettiger <cboettig@gmail.com>
2014-09-30 16:17:17 -0700

    tweak test script

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,3 +1,4 @@
 
-docker run --rm -it eddelbuettel/debian-r-base Rscript -e 'sapply(installed.packages()[,"Package"], tools::testInstalledPackage)'
-docker run --rm -it eddelbuettel/debian-hadleyverse Rscript -e 'sapply(installed.packages()[,"Package"], tools::testInstalledPackage)'
+docker run --rm -it eddelbuettel/debian-r-base Rscript --default-packages="stats, graphics, grDevices, datasets, utils, methods, base" -e 'sapply(installed.packages()[,"Package"], tools::testInstalledPackage, type=c("examples", "tests"))'
+
+docker run --rm -it eddelbuettel/debian-hadleyverse Rscript  --default-packages="stats, graphics, grDevices, datasets, utils, methods, base" -e 'sapply(c("devtools", "ggplot2" "dplyr", "tidyr", "reshape2", "roxygen2", "knitr", "testthat", "rmarkdown", "httr", "Rcpp"), tools::testInstalledPackage, type=c("examples", "tests"))'
